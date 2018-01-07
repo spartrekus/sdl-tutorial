6649ca4d9ea88dc82a0e90ad3c8621934f13c2c3
spartrekus <spartrekus@gmail.com>
2018-01-07 15:33:42 +0000

    Add files via upload

---
+++ 07.sdl.blue.background.c
@@ -0,0 +1,55 @@
+
+// bring blue color the whole  screen 
+
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+int main(int argc, char *argv[]) 
+{
+
+    SDL_Event event;
+    SDL_Init(SDL_INIT_VIDEO); //Initialisation de la SDL
+    SDL_WM_SetCaption("Hello World! :D", NULL);
+
+    SDL_putenv("SDL_VIDEO_CENTERED=center"); // Centre la fenetre a l'écran
+     
+    SDL_Surface *screen; // create screen
+    screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE); // charge la fenetre avec sa taille et ses flags
+
+     
+    int gameover = 0 ; 
+    while( gameover == 0 )
+    {
+         SDL_WaitEvent(&event);
+
+         // make blue background
+         SDL_FillRect( screen,  0 , SDL_MapRGB( screen->format, 0, 0, 255  ) );
+
+
+         // draw pixels to screen 
+         SDL_Flip( screen );
+
+
+         if ( event.type == SDL_QUIT )
+            gameover = 1;
+         
+         switch(event.type)
+         {
+             case SDL_KEYDOWN:
+               printf( "EVENT KEYDOWN...\n" );
+               gameover = 1;
+               break;
+
+             case SDL_QUIT:
+               printf( "EVENT QUIT...\n" );
+               gameover = 1;
+               break;
+         }
+     }
+
+     printf( "Bye...\n" );
+
+     SDL_Quit(); // Quitte les systemes de la sdl
+     return EXIT_SUCCESS; // tout c'est bien passé
+}
+
